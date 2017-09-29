36d666ded441616c6589893fdcaf23feaca7f2a5
marianosalem <mariano.salem@frogslayer.com>
2017-09-29 13:51:41 -0500

    Added more style into header

--- src/components/Header.js
+++ src/components/Header.js
@@ -19,9 +19,11 @@ const styles = {
         justifyContent: 'center',
         alignItems: 'center',
         paddingTop: 16,
-        shadowColor: "#EEEEEE",
+        shadowColor: "#000",
         shadowOffset: { width: 0, height: 2},
-        shadowOpacity: 0.2
+        shadowOpacity: 0.2,
+        elevation: 2, 
+        position: 'relative'
     },
     textStyle: {
         fontSize: 20
