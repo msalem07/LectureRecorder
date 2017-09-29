d8ad1c44ce8daae7ddfcb0514bc43067d11d1850
marianosalem <mariano.salem@frogslayer.com>
2017-09-29 13:49:15 -0500

    Added Header file

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function Header () {
+
+    const { textStyle, viewStyle } = styles;
+
     return (
-        <Text>HEADER!!</Text>
-    )
+        <View style={viewStyle}>
+            <Text style={textStyle}>HEADER!!</Text>
+        </View>
+    );
+}
+
+const styles = {
+    viewStyle: {
+        backgroundColor: "#F8F8F8",
+        height: 60,
+        justifyContent: 'center',
+        alignItems: 'center',
+        paddingTop: 16,
+        shadowColor: "#EEEEEE",
+        shadowOffset: { width: 0, height: 2},
+        shadowOpacity: 0.2
+    },
+    textStyle: {
+        fontSize: 20
+    }
 }
